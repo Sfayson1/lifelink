4e741a80459eae946ae92ae1363c055fbf67d64c
James Bratcher <jbratch90@gmail.com>
2024-01-20 10:21:14 -0700

    Worked on create_post endpoint, requires further work, see James_bratcher.MD

--- api/routers/posts.py
+++ api/routers/posts.py
@@ -5,9 +5,10 @@ from fastapi import (
     APIRouter,
 
 )
-from models import PostIn, PostOut
+from models import PostIn, PostOut, PostList
 from queries.posts import PostQueries
 from pydantic import BaseModel
+from authenticator import authenticator
 
 
 router = APIRouter()
@@ -16,31 +17,33 @@ router = APIRouter()
 class HttpError(BaseModel):
     detail: str
 
-
-@router.get("/posts/", response_model=PostOut)
-async def list_posts(
+@router.get("/posts/", response_model=PostList)
+async def list_users_posts(
     post_id: int,
     repo: PostQueries = Depends(),
 ):
     return repo.get_post(post_id)
 
-@router.post("/posts/")
-async def create_post(
-    post: PostIn,
-    repo: PostQueries = Depends(),
-):
-    return repo.create_post(post)
-
-
-@router.delete("/posts/{post_id}")
-async def delete_post(
+@router.get("/posts/mine", response_model=PostList)
+async def list_my_posts(
     post_id: int,
     repo: PostQueries = Depends(),
 ):
-    deleted = repo.delete_post(post_id)
-    if not deleted:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Post not found",
-        )
-    return {"message": "Post deleted successfully"}
+    return repo.get_post(post_id)
+
+@router.post("/posts", response_model=PostOut)
+async def create_post(
+    post: PostIn,
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: PostQueries = Depends(),
+):
+    user_id = account_data.get("user_id")
+    return repo.create_post(post)
+
+@router.delete("/posts/{post_id}/", response_model=bool)
+async def delete_post(
+    post_id: int,
+    repo: PostQueries = Depends(),
+) ->bool:
+    return repo.dete_post(post_id)
+    
