b7201c6fa17032c050d785c927a62ded03fe7d01
Zack Hitchcock <hitchcockzack@gmail.com>
2024-01-25 17:27:44 -0500

    navbar, docker compose corrections for frontend.

--- api/routers/posts.py
+++ api/routers/posts.py
@@ -33,8 +33,8 @@ async def list_my_posts(
     account_data: dict = Depends(authenticator.get_current_account_data),
     repo: PostQueries = Depends(),
 ):
-    return repo.get_user_posts(account_data['username'])
-    # return repo.get_user_posts(username=account_data['username'])
+    # return repo.get_user_posts(account_data['username'])
+    return repo.get_user_posts(username=account_data['username'])
 
 @router.post("/posts", response_model=PostOut)
 async def create_post(
