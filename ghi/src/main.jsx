0b072d0e032740593392deccb426ca10876b3c14
James Bratcher <jbratch90@gmail.com>
2024-01-30 08:55:15 -0700

    Working Router, Currently an error in browser console upon use

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -1,17 +1,12 @@
-//@ts-check
+// @ts-nocheck
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Nav from './navbars/Mainnavbar.jsx'
-import Login from './login.jsx'
-import Signup from './Signup.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Nav />
-        {/* <App /> */}
-        <Signup />
-        <Login />
+        <App />
     </React.StrictMode>
 )
