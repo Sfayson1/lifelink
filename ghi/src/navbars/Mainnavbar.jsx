7e814fce933aebe3947ebf8c9408d3cd0019e7eb
James Bratcher <jbratch90@gmail.com>
2024-01-30 23:14:34 +0000

    Update Mainnavbar.jsx

--- ghi/src/navbars/Mainnavbar.jsx
+++ ghi/src/navbars/Mainnavbar.jsx
@@ -7,8 +7,9 @@ const Nav = () => {
         <nav>
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href='/signup'>Contact</a></li>
+                <li><a href="/Login">Log in</a></li>
+                <li><a href="/Signup">Sign up</a></li>
+                <li><a href="/Update">Update</a></li>
             </ul>
         </nav>
     );
