44e896c06a90041e25577b014bf0517f5b5c034b
Sherika <faysons81@gmail.com>
2024-01-31 11:28:52 -0500

    submit button redirect

--- ghi/src/Homepage.jsx
+++ ghi/src/Homepage.jsx
@@ -1,47 +1,55 @@
-import React from 'react';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Homepage = () => {
+    const navigate = useNavigate()
 
     const navigateToSignup = () => {
-        history.push('/signup');
-    };
+        navigate('/signup')
+    }
 
     return (
-        <div> <input
-        placeholder="What's happening now?!"/>
-        <button onClick={navigateToSignup}> submit </button>
+        <div>
+            {' '}
+            <input placeholder="What's happening now?!" />
+            <button onClick={navigateToSignup}> submit </button>
             <div className="card">
                 <div className="cardbody">
                     <h5>Johnnyeta2024</h5>
                     <span>
-                        < small>1h</small>
+                        <small>1h</small>
                     </span>
-                    <p >"This is how life is going after Hack Reactor. Tell us a great story or two and where you are at in life."</p>
-
+                    <p>
+                        "This is how life is going after Hack Reactor. Tell us a
+                        great story or two and where you are at in life."
+                    </p>
                 </div>
             </div>
             <div className="card">
                 <div className="cardbody">
                     <h5>Johnnyeta2024</h5>
                     <span>
-                        < small>1h</small>
+                        <small>1h</small>
                     </span>
-                    <p >"What is your current position and company? Any opportunities for other alumni?"</p>
-
+                    <p>
+                        "What is your current position and company? Any
+                        opportunities for other alumni?"
+                    </p>
                 </div>
             </div>
             <div className="card">
                 <div className="cardbody">
                     <h5>Johnnyeta2024</h5>
                     <span>
-                        < small>1h</small>
+                        <small>1h</small>
                     </span>
-                    <p >"Tell us if you are enjoying life after Hack Reactor!"</p>
-
+                    <p>
+                        "Tell us if you are enjoying life after Hack Reactor!"
+                    </p>
                 </div>
             </div>
-
-        </div>)
+        </div>
+    )
 }
 
-export default Homepage;
+export default Homepage
