11aeef89034800a116a56b9ad6a4287db06a6e96
James Bratcher <jbratch90@gmail.com>
2024-02-01 16:25:04 -0700

    Delete user added, consider putting a popup box to confirm selection. Login user redirect started, login works, but redirect does not.

--- ghi/src/login.jsx
+++ ghi/src/login.jsx
@@ -1,26 +1,55 @@
-import { useState } from 'react';
-import useToken from "@galvanize-inc/jwtdown-for-react";
-
-
-
-
+import { useState, useEffect } from 'react';
+import  useToken from "@galvanize-inc/jwtdown-for-react";
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [token, setToken] = useState('');
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
   const { login } = useToken();
+  const navigate = useNavigate();
+
+  const fetchToken = async () => {
+    const tokenURL = "http://localhost:8000/token";
+    const fetchConfig = {credentials:'include'};
+    const response = await fetch(tokenURL, fetchConfig);
+    if (response.ok) {
+      const data = await response.json();
+      if (!data){
+        return null;
+      }
+      setToken(data.access_token)
+      console.log(data.access_token)
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await login(username, password);
-    e.target.reset();
-  };
+    try {
+      login(username,password);
+      console.log('*data*', token)
+      if (token !== null){
+        e.target.reset();
+        navigate('/');
+      } else if (token === null) {
+        setErrorMsg("Login failed. Please check your username and password.");
+      }
+    } catch (error) {
+      console.error("Login failed:", error.message);
+      setErrorMsg("Login failed. Please check your username and password.");
+      }
+    };
 
+  useEffect(() => {
+      fetchToken();
+  }, [])
 
   return (
     <div className="card text-bg-light mb-3">
       <h5 className="card-header">Login</h5>
       <div className="card-body">
+        {errorMsg && <div className="alert alert-danger">{errorMsg}</div>}
         <form onSubmit={(e) => handleSubmit(e)}>
           <div className="mb-3">
             <label className="form-label">Username:</label>
@@ -41,7 +70,11 @@ const Login = () => {
             />
           </div>
           <div>
-            <input className="btn btn-primary" type="submit" value="Login" />
+            <input
+            className="btn btn-primary"
+            type="submit"
+            value="Login"
+            />
           </div>
         </form>
       </div>
