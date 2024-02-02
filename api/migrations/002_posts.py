278e4b5741e28aa92b19443a0fedeee2e62653d6
Zack Hitchcock <hitchcockzack@gmail.com>
2024-02-02 16:10:50 -0500

    posts migration corrected to account for changes made in profile page

--- api/migrations/002_posts.py
+++ api/migrations/002_posts.py
@@ -6,7 +6,9 @@ steps=[
             id SERIAL PRIMARY KEY,
             content VARCHAR,
             date_posted DATE,
-            user_id INT
+            user_id INT,
+            user_first_name VARCHAR(255),
+            user_last_name VARCHAR(255),
         );
         """,
         #step 2
