45ba7d803b6ef46e0f3d14a7ca0a0a78b0eed227
Zack Hitchcock <hitchcockzack@gmail.com>
2024-02-05 14:14:46 -0500

    all recent team changes integrated

--- api/migrations/002_posts.py
+++ api/migrations/002_posts.py
@@ -2,14 +2,12 @@ steps=[
     [
         #step 1
         """
-        CREATE TABLE IF NOT EXISTS posts (
+        CREATE TABLE posts (
             id SERIAL PRIMARY KEY,
             content VARCHAR,
             date_posted DATE,
             user_id INT,
-            CONSTRAINT fk_user
-                FOREIGN KEY(user_id)
-                    REFERENCES users(id)
+            FOREIGN KEY (user_id) REFERENCES users(id)
         );
         """,
         #step 2
