e3ebc8268d4e7398634457127bfab3bd7b86e65e
Zack Hitchcock <hitchcockzack@gmail.com>
2024-02-08 12:03:26 -0500

    Home page style update

--- ghi/src/Home.jsx
+++ ghi/src/Home.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import './Home.css'
 const Home = () => {
     const [posts, setPosts] = useState([])
     const [newPost, setNewPosts] = useState('')
