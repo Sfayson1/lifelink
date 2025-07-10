bea1dd846777d588f5d433a9c10222390b32fdee
Sherika Fayson <137829671+Sfayson1@users.noreply.github.com>
2025-07-09 22:30:17 -0400

    Update vite.config.js

--- ghi/vite.config.js
+++ ghi/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        strictPort: true,
-        watch: {
-          usePolling: true
-        }
-    }
+  plugins: [react()],
+  base: '/lifelink/', 
+  build: {
+    outDir: 'dist'
+  }
 })
