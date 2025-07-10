5df951b6300a394e175ee7b7bf077c6cea72b77b
Sherika Fayson <137829671+Sfayson1@users.noreply.github.com>
2025-07-10 03:34:33 -0400

    Update vite.config.js

--- ghi/vite.config.js
+++ ghi/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/', // Root path for Render static sites
   build: {
     outDir: 'dist'
+  },
+  server: {
+    port: 5173
   }
 })
