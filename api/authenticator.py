91338d541735a75bd9adc8b69767442b081ac790
Sherika Fayson <137829671+Sfayson1@users.noreply.github.com>
2025-07-10 18:44:29 -0400

    Update authenticator.py

--- api/authenticator.py
+++ api/authenticator.py
@@ -1,4 +1,30 @@
 import os
+from fastapi import Depends
 from jwtdown_fastapi.authentication import Authenticator
+from queries.users import UserQueries
+from models import UserOutWithPassword, UserOut
 
-authenticator = Authenticator(os.environ["SIGNING_KEY"])
+
+class LifeLinkAuthenticator(Authenticator):
+    async def get_account_data(
+        self,
+        username: str,
+        accounts: UserQueries,
+    ):
+        return accounts.get(username)
+
+    def get_account_getter(
+        self,
+        accounts: UserQueries = Depends(),
+    ):
+        return accounts
+
+    def get_hashed_password(self, account: UserOutWithPassword):
+        return account.hashed_password
+
+    def get_account_data_for_cookie(self, account: UserOutWithPassword):
+        return account.username, UserOut(**account.dict())
+
+
+# ✅ Instantiate your subclass, not the abstract base
+authenticator = LifeLinkAuthenticator(os.environ["SIGNING_KEY"])
