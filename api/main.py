3a3bbb7692c68cdce273619dd1e36e01a5979002
Sherika Fayson <137829671+Sfayson1@users.noreply.github.com>
2025-07-10 02:24:30 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,19 +12,11 @@ app.add_middleware(
     allow_origins=[
         "https://lifelink-mv0n.onrender.com",
         "http://localhost:5173",
-        "http://localhost:3000",
+        "http://localhost:3000"
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=[
-        "Accept",
-        "Accept-Language",
-        "Content-Language",
-        "Content-Type",
-        "Authorization",
-        "Cookie",
-        "Set-Cookie",
-    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(posts.router, tags=["Posts"])
