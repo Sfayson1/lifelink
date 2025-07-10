c14e027f60f0dbf2775eefb13a25f2333837a476
Sherika Fayson <137829671+Sfayson1@users.noreply.github.com>
2025-07-10 00:08:37 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "http://localhost:5173",             
         "http://localhost:3000",             
     ],
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
